1803ae0d571919a24fdd49b09d7e096dc9e2c40a
hans <hans.git@furg.br>
2022-05-06 10:56:42 -0300

    new test

--- test/failing-test-spec.js
+++ test/failing-test-spec.js
@@ -15,4 +15,36 @@ describe('test', function () {
   it('should return Ridgewood', function () {
     expect(getCheapestHotel("Rewards: 26Mar2009(thur), 27Mar2009(fri), 28Mar2009(sat)")).to.equal("Ridgewood");
   });
+  it('should return Lakewood', function () {
+    expect(getCheapestHotel("Rewards: 16Mar2009(mon), 17Mar2009(tues), 18Mar2009(wed)")).to.equal("Lakewood");
+  });
+  it('should return Ridgewood', function () {
+    expect(getCheapestHotel("Rewards: 20Mar2009(fri), 21Mar2009(sat), 22Mar2009(sun)")).to.equal("Ridgewood");
+  });
+  it('should return Lakewood', function () {
+    expect(getCheapestHotel("Regular: 26Mar2009(thur), 27Mar2009(fri), 28Mar2009(sat)")).to.equal("Lakewood");
+  });
+  it('One week regular: should return Lakewood', function () {
+    expect(getCheapestHotel("Regular: 16Mar2009(mon), 17Mar2009(tues), 18Mar2009(wed), 19Mar2009(thur), 20Mar2009(fri), 21Mar2009(sat), 22Mar2009(sun)")).to.equal("Lakewood");
+  });
+  it('One week reward: should return Lakewood', function () {
+    expect(getCheapestHotel("Rewards: 16Mar2009(mon), 17Mar2009(tues), 18Mar2009(wed), 19Mar2009(thur), 20Mar2009(fri), 21Mar2009(sat), 22Mar2009(sun)")).to.equal("Lakewood");
+  });
+  it('One weekday regular: should return Lakewood', function () {
+    expect(getCheapestHotel("Regular: 20Mar2009(fri)")).to.equal("Lakewood");
+  });
+  it('One weekday reward: should return Lakewood', function () {
+    expect(getCheapestHotel("Rewards: 20Mar2009(fri)")).to.equal("Lakewood");
+  });
+  it('One weekend regular: should return Bridgewood', function () {
+    expect(getCheapestHotel("Regular: 20Mar2009(sat)")).to.equal("Bridgewood");
+  });
+  it('One weekend reward: should return Ridgewood', function () {
+    expect(getCheapestHotel("Rewards: 20Mar2009(sat)")).to.equal("Ridgewood");
+  });
+
+
+  it('should return Invalid Input', function () {
+    expect(getCheapestHotel()).to.equal("Invalid Input!");
+  });
 })
